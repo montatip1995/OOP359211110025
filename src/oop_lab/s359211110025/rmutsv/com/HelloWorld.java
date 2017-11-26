aa47b7cf5e8bed091cc5889f4ed9303c7a769f4d
montatip1995 <tan.montatip@gmail.com>
2017-11-26 20:49:08 +0700

    Initial commit

---
+++ src/oop_lab/s359211110025/rmutsv/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package oop_lab.s359211110025.rmutsv.com;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world,I'm Tan");
+        System.out.println("My name is Montatip Yodsawai");
+//    add information
+        System.out.println("Faculty: Mangement Technology,RMUTSV");
+    }//main
+}//class
