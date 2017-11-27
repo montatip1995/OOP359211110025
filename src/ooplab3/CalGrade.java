d7e526ef6db507826e7490d7dd2c6f22931cc8b6
montatip1995 <tan.montatip@gmail.com>
2017-11-27 14:58:51 +0700

    TestCondition

---
+++ src/ooplab3/CalGrade.java
@@ -0,0 +1,15 @@
+package ooplab3;
+
+public class CalGrade {
+//    score 0-49 grade F
+//    score 50-59 grade D
+//    score 60-69 grade C
+//    score 70-79 grade B
+//    score 80-100 grade A
+public static void main(String[] args) {
+
+
+
+}   //main
+
+}
