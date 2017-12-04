5c6d640ade4a869fbf24b0d92500b2db7e7be790
montatip1995 <tan.montatip@gmail.com>
2017-12-04 15:50:26 +0700

    OverloadMethods

---
+++ src/ooplab4/OverloadMethods.java
@@ -0,0 +1,30 @@
+package ooplab4;
+
+public class OverloadMethods {
+    private static int value=100;
+
+        public static void A(){
+            System.out.println("Hello");
+            int number=20;
+            System.out.println(number);
+            System.out.println(value);
+        }
+        public static void A(int x){
+            System.out.println("Hello"+x);
+        }
+        public static void A(int x,int y){
+            System.out.println("Hello"+(x+y));
+        }
+
+    public static void main(String[] args) {
+        //        OverloadMethods
+            A();
+            A(5);
+            A(5,10);
+
+//            local variable
+        int number=10;
+
+
+    }//main
+}//class
