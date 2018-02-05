925229a56b885340dfc0dadeb4e02f8f9174c625
montatip1995 <tan.montatip@gmail.com>
2018-02-05 18:15:01 +0700

    PersonApp

--- src/ooplab9/PersonApp.java
+++ src/ooplab9/PersonApp.java
@@ -2,7 +2,7 @@ package ooplab9;
 
 public class PersonApp {
     person person = new person("Montatip Yodsawai","1111111111111",
-            new Address ("272 M.12","NakonSriThammarat","92110"),
+            new Address ("272 M.12","NakonSriThammarat","80230"),
             new Job("Student","7000"));
 
         System.out.println(person.toString());
